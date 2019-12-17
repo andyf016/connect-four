9a41cd96f588cd51fe5ca7fc07768b9bf2abe664
Andrew Fillenwarth <andyf016@gmail.com>
2019-12-17 14:12:44 -0500

    update outline

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 /*
 1. Display connect 4 grid on screen 
     a. click handlers
-    b. some kind of CSS
+    b. Beautiful CSS
         -flex boxes
-    c. no invalis moves 
+        -colors
+        -grid
+    c. No invalid moves 
         -display ID row div
         -click counter 
         -once a column is full, cant add more disks 
 2. Turn based 
-    a. keep track of disck location
-    -Cycles colors based on input counter
+    a. Keep track of disck location
+        -Cycles colors based on input counter
+    b. Properly cycle between colors 
 3. Win condition
     a. 1 player has 4 colors in a row or diagnaly 
-    b.  
+    b. Program recognizes win
         -2 variables, "or" command to allow only 1 color to activate win condition   
 
     */ 
