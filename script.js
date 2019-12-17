fe6b4fb3f2d7641e4bd6b52333f1b7299c4544a7
Andrew Fillenwarth <andyf016@gmail.com>
2019-12-17 13:27:29 -0500

    commit 1st outline

---
+++ script.js
@@ -0,0 +1,18 @@
+/*
+1. Display connect 4 grid on screen 
+    a. click handlers
+    b. some kind of CSS
+        -flex boxes
+    c. no invalis moves 
+        -display ID row div
+        -click counter 
+        -once a column is full, cant add more disks 
+2. Turn based 
+    a. keep track of disck location
+    -Cycles colors based on input counter
+3. Win condition
+    a. 1 player has 4 colors in a row or diagnaly 
+    b.  
+        -2 variables, "or" command to allow only 1 color to activate win condition   
+
+    */ 
